31a1ac46cfbc08f05e05bab604312dfe024e7d26
Sahar Galimidi <sahargalimidi@gmail.com>
2023-12-21 14:47:51 +0200

    Pipeline Check

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from first_ci_cd import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), 'Hello, World!')
+        self.assertEqual(hello_world(), 'Check if its working')
 
 if __name__ == '__main__':
     unittest.main()
