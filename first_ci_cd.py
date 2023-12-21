bca461318b4c3ab6e4d1968d14f1f0eb487c3534
Sahar Galimidi <sahargalimidi@gmail.com>
2023-12-21 14:51:50 +0200

    Pipeline Check

--- first_ci_cd.py
+++ first_ci_cd.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Check if its working'
+    return 'Check if its working2'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from first_ci_cd import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), 'Check if its working')
+        self.assertEqual(hello_world(), 'Check if its working2')
 
 if __name__ == '__main__':
     unittest.main()
