ea847992fcbc0a1c4d7c97e02790ef0752b80670
Sahar Galimidi <sahargalimidi@gmail.com>
2023-12-22 10:19:29 +0200

    Trying with ngrok

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from first_ci_cd import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), 'Check if its working2')
+        self.assertEqual(hello_world(), 'Check if its working3')
 
 if __name__ == '__main__':
     unittest.main()
